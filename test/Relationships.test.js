bac7fb13d6c74b8067266e43df07aeafe4a3f9ec
Charles Bido <charlesbido@me.com>
2016-05-13 01:13:00 -0500

    create Relationships object

---
+++ test/Relationships.test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let should = require('chai').should();
+let Relationships = require('../lib/Relationships.js');
+
+describe("Relationships", function() {
+  describe("#links", function() {
+    it('should be a links object');
+  });
+  describe("#data", function() {
+    it("should be a resource linkage");
+  });
+  describe("#meta", function() {
+    it('should be a meta object');
+  });
+});
